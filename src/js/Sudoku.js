e7a259c5b2d5fedb188cacd83f917703b690a700
Vincent B <vin100.beck@gmail.com>
2016-02-27 13:55:18 -0500

    isValid

--- src/js/Sudoku.js
+++ src/js/Sudoku.js
@@ -7,8 +7,7 @@ var Sudoku = (function ($) {
     /**
      * Configuration information
      */
-    var config = {
-    };
+    var config = {};
 
     /**
      * Contains sudoku values
@@ -30,7 +29,7 @@ var Sudoku = (function ($) {
      * Get sudoku HTML
      * @returns {*|jQuery|HTMLElement}
      */
-    var getSudokuHtml = function () {
+    var _getSudokuHtml = function () {
         var content = '';
         for (var i = 0; i < 9; i++) {
             content += '<tr>';
@@ -41,6 +40,33 @@ var Sudoku = (function ($) {
         }
 
         return $('<table>' + content + '</table>');
+    };
+
+    /**
+     * Is a subset valid ?
+     * Check that every value is unique in a subset, check that every value is a digit as well
+     * @param subset
+     * @returns {boolean}
+     * @private
+     */
+    var _isSubsetValid = function (subset) {
+        var tmpSubset = {};
+        for (var i = 0; i < subset.length; i++) {
+            // Is it a digit ?
+            if (!subset[i].toString().match(/^[0-9]$/)) {
+                return false;
+            }
+
+            if (subset[i] in tmpSubset) {
+                // The value already exist in the subset
+                return false;
+
+            } else {
+                tmpSubset[subset[i]] = 1;
+            }
+        }
+
+        return true;
     };
 
     /*******
@@ -56,7 +82,60 @@ var Sudoku = (function ($) {
             throw 'The wrapper does not exist';
         }
 
-        wrapper.html(getSudokuHtml());
+        wrapper.html(_getSudokuHtml());
+    };
+
+    /**
+     * Is the sudoku valid?
+     * @returns {boolean}
+     */
+    Sudoku.prototype.isValid = function () {
+        var rows = [];
+        var cols = [];
+        var squares = {};
+
+        for (var i = 0; i < 9; i++) {
+            // Row
+            if (i in values) {
+                rows.push(values[i]);
+            }
+
+            var col = [];
+            for (var j = 0; j < 9; j++) {
+                // Column
+                if (j in values && i in values[j]) {
+                    col.push(values[j][i]);
+                }
+
+                // Square
+                if (i in values && j in values[i]) {
+                    var key = i % 3 + '|' + j % 3;
+                    if (!key in squares) {
+                        squares[key] = [];
+                    }
+
+                    squares[key] = values[i][j];
+                }
+            }
+
+            cols.push(col);
+        }
+
+        var subsets = rows.concat(cols, Object.keys(squares).map(function(key){return squares[key]}));
+        for (var i = 0; i < subsets.length; i++) {
+            if (!_isSubsetValid(subsets[i])) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    /**
+     * Set sudoku values
+     * @param v: values
+     */
+    Sudoku.prototype.setValues = function (v) {
+        values = v;
     };
 
     return Sudoku;
